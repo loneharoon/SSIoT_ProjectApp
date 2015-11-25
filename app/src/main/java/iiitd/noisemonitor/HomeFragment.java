bb4545f7c15a82fb2c685e9ceccdd3401118a272
GarvitaBajaj <garvitab@iiitd.ac.in>
2015-11-25 12:02:49 +0530

    fetch sensor data every 5 seconds

--- app/src/main/java/iiitd/noisemonitor/HomeFragment.java
+++ app/src/main/java/iiitd/noisemonitor/HomeFragment.java
@@ -1,12 +1,16 @@
 package iiitd.noisemonitor;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ubidots.ApiClient;
 import com.ubidots.Value;
@@ -21,19 +25,55 @@ import java.util.Date;
 public class HomeFragment extends android.app.Fragment {
 
 	private TextView mnoise1,mnoise2;
+	private Button start,stop;
+	Context mContext;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		setHasOptionsMenu(true);
 		setRetainInstance(true);
 
+		mContext=getActivity();
 		View view= inflater.inflate(R.layout.content_main, container, false);
 
 		mnoise1 = (TextView) view.findViewById(R.id.noiseLevel);
 		mnoise2 = (TextView) view.findViewById(R.id.noiseLevel2);
-		new ApiUbidots().execute();
+		start=(Button)view.findViewById(R.id.button);
+		stop=(Button)view.findViewById(R.id.button2);
+
+		start.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				mHandlerTask.run();
+				Toast.makeText(mContext,"Fetching sensor readings now",Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		stop.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mHandler.removeCallbacks(mHandlerTask);
+				Toast.makeText(mContext,"Sensor readings stopped",Toast.LENGTH_SHORT).show();
+			}
+		});
+//		new ApiUbidots().execute();
 		return view;
 	}
+
+	private final static int INTERVAL = 1000 * 5; //5 seconds
+	Handler mHandler=new Handler();
+
+	Runnable mHandlerTask = new Runnable()
+	{
+		@Override
+		public void run() {
+			new ApiUbidots().execute();
+			mHandler.postDelayed(mHandlerTask, INTERVAL);
+		}
+	};
 
 	public class ApiUbidots extends AsyncTask<Void, Void, Void> {
 		private final String API_KEY = "b107bfa74890be5625245c25ad261cbeafa1f135";
@@ -50,8 +90,8 @@ public class HomeFragment extends android.app.Fragment {
 			Variable noiseLevel2 = apiClient.getVariable(VARIABLE_ID_S2);
 
 			final Value[] val = noiseLevel.getValues();
-			final Value[] val2=noiseLevel2.getValues();
-			String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ").format(new Date(val[1].getTimestamp()));
+			final Value[] val2 = noiseLevel2.getValues();
+			String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ").format(new Date(val[ 1 ].getTimestamp()));
 			Log.i("info", dateString);
 			getActivity().runOnUiThread(new Runnable() {
 											@Override
@@ -63,9 +103,17 @@ public class HomeFragment extends android.app.Fragment {
 			);
 			Log.i("info", "value from sensor 1:" + val[ 1 ].getValue());
 			Log.i("info","value from sensor 2:"+val2[2].getValue());
+			Log.i("refreshed","values fetched again");
 			return null;
 		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			Log.d("Ubidots", "inside onPostExecute now");
+		}
 	}
+
+
 
 	@Override
 	public void onStart() {
